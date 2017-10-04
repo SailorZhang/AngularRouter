8a059f5fa505dbcf42b425704ee452eb8dd1e736
xiangnan.zhang <z@zhangxiangnan.cn>
2017-10-04 20:42:01 +0800

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {StockComponent} from "./stock/stock.component";
+import {Code404Component} from "./code404/code404.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'stock/:id',
+    component: StockComponent,
+    data:[{isPro:true}]
+  },
+  {
+    path: '**',
+    component: Code404Component
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ src/app/stock/stock.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+
+@Component({
+  selector: 'app-stock',
+  templateUrl: './stock.component.html',
+  styleUrls: ['./stock.component.css']
+})
+export class StockComponent implements OnInit {
+
+  stockId:number;
+
+  isPro:boolean;
+
+  constructor(private routeInfo: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.routeInfo.params.subscribe((params:Params)=>{
+      this.stockId = params["id"];
+    });
+
+    this.isPro = this.routeInfo.snapshot.data[0]["isPro"];
+  }
+
+}
